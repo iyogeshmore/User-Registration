64fc46cec7a2edb6df9565231d16e9a776a01fb8
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-17 15:28:40 +0530

    Added : Exception for all test cases

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -69,7 +69,7 @@ public class UserRegistration {
             System.out.println("It is invalid Mobile Number.");
     }
 
-    public static void validatePasswordRule1(){
+    public static void validatePasswordRule1() {
         System.out.println("Enter Your Unique Password :  ");
         String password = sc.nextLine();
         String regex4 = "^[A-Z a-z 0-9]{8,}$";
@@ -84,7 +84,7 @@ public class UserRegistration {
 
     }
 
-    public static void validatePasswordRule2(){
+    public static void validatePasswordRule2() {
         System.out.println("Enter The Password At least Eight Character one Upper case: ");
         String password = sc.nextLine();
         String regex5 = "^[A-Z]{1}+[a-z 0-9]{7,}$";
@@ -98,7 +98,8 @@ public class UserRegistration {
             System.out.println("It is invalid Password.");
 
     }
-    public static void validatePasswordRule3(){
+
+    public static void validatePasswordRule3() {
         System.out.println("Enter The Password At least one Upper case and one numeric");
         String password = sc.nextLine();
         String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]+{1,}$";
@@ -107,11 +108,13 @@ public class UserRegistration {
         boolean r = matcher6.matches();
 
         if (r)
-            System.out.println("It is a valid Password.");
+            System.out.println("It is a vali" +
+                    "d Password.");
         else
             System.out.println("It is invalid Password.");
 
     }
+
     public static void validatePasswordRule4() {
         System.out.println("Enter the Password At least One Upper Case & One Numeric & One Special Character:");
         String password = sc.nextLine();
@@ -125,6 +128,7 @@ public class UserRegistration {
         else
             System.out.println("It is invalid Password.");
     }
+
     public static void emailSamples() {
         System.out.print("Enter the email :");
         String emailSample = sc.next();
@@ -140,95 +144,215 @@ public class UserRegistration {
 
     //Junit UC1: Take First Name And Check It Is Getting Matched With Regex/Regular
 
-    public boolean validateFirstName(String fname) {
-        String regex="^[A-Z]{1}[a-z]*";
-        Pattern p=Pattern.compile(regex);
-        Matcher m=p.matcher(fname);
-        boolean r=m.matches();
-        return r;
+//    public boolean validateFirstName(String fname) {
+//        String regex = "^[A-Z]{1}[a-z]*";
+//        Pattern p = Pattern.compile(regex);
+//        Matcher m = p.matcher(fname);
+//        boolean r = m.matches();
+//        return r;
+//    }
+
+    //Exception Handled for First name : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex = "^[A-Z]{1}[a-z]*";
+
+    public boolean validateFirstName(String fName) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex);
+        if (Pattern.matches(regex, fName) == true)
+            return Pattern.matches(regex, fName);
+        else {
+            throw new UserRegistrationException("Send a proper First Name");
+        }
     }
 
     //Junit UC2: Take Last Name And Check It Is Getting Matched With Regex/Regular
 
-    public boolean validateLastName(String lName) {
-        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern1 = Pattern.compile(regex1);
-        Matcher matcher1 = pattern1.matcher(lName);
-        boolean r = matcher1.matches();
-        return r;
+//    public boolean validateLastName(String lName) {
+//        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
+//        Pattern pattern1 = Pattern.compile(regex1);
+//        Matcher matcher1 = pattern1.matcher(lName);
+//        boolean r = matcher1.matches();
+//        return r;
+//    }
+
+    //Exception Handled for Last name : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex1 = "^[A-Z]{1}[a-z]*";
+
+    public boolean validateLastName(String lName) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex1);
+        if (Pattern.matches(regex1, lName) == true)
+            return Pattern.matches(regex1, lName);
+        else {
+            throw new UserRegistrationException("Send a proper Last Name");
+        }
     }
 
     //Junit UC3: Take Email And Check It Is Getting Matched With Regex/Regular
 
-    public boolean validateEmail(String email) {
-        String regex2 = "^[a-zA-Z0-9]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
-        // {1,}^[a-z]+[0-9]+[@]+gmail.com
-        Pattern pattern2 = Pattern.compile(regex2);
-        Matcher matcher2 = pattern2.matcher(email);
-        boolean r = matcher2.matches();
-        return r;
+//        public boolean validateEmail (String email){
+//            String regex2 = "^[a-zA-Z0-9]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
+//            // {1,}^[a-z]+[0-9]+[@]+gmail.com
+//            Pattern pattern2 = Pattern.compile(regex2);
+//            Matcher matcher2 = pattern2.matcher(email);
+//            boolean r = matcher2.matches();
+//            return r;
+//        }
+
+    //Exception Handled for Email : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex2 = "^[a-zA-Z0-9]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
+
+    public boolean validateEmail(String email) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex1);
+        if (Pattern.matches(regex2, email) == true)
+            return Pattern.matches(regex2, email);
+        else {
+            throw new UserRegistrationException("Send a proper Email");
+        }
     }
 
     //Junit UC4: Take Mobile Number And Check It Is Getting Matched With Regex/Regular
 
-    public boolean validateMobileNumber(String mobileNo) {
-        String regex3 = "(91|0)?\\s?[6-9][0-9]{9}$";
-        Pattern pattern3 = Pattern.compile(regex3);
-        Matcher matcher3 = pattern3.matcher(mobileNo);
-        boolean r = matcher3.matches();
-        return r;
+//        public boolean validateMobileNumber (String mobileNo){
+//            String regex3 = "(91|0)?\\s?[6-9][0-9]{9}$";
+//            Pattern pattern3 = Pattern.compile(regex3);
+//            Matcher matcher3 = pattern3.matcher(mobileNo);
+//            boolean r = matcher3.matches();
+//            return r;
+//        }
+
+//Exception Handled for Mobile Number : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex3 = "(91|0)?\\s?[6-9][0-9]{9}$";
+
+    public boolean validateMobileNumber(String mobileNo) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex3);
+        if (Pattern.matches(regex3, mobileNo) == true)
+            return Pattern.matches(regex3, mobileNo);
+        else {
+            throw new UserRegistrationException("Send a proper Mobile Number");
+        }
     }
 
     //Junit UC5: Take Password (Minimum 8 characters) And Check It Is Getting Matched With Regex/Regular
 
-    public boolean validatePasswordRule1(String password) {
-        String regex4 = "^[A-Z a-z 0-9]{8,}$";
-        Pattern pattern4 = Pattern.compile(regex4);
-        Matcher matcher4 = pattern4.matcher(password);
-        boolean r = matcher4.matches();
-        return r;
+//        public boolean validatePasswordRule1 (String password){
+//            String regex4 = "^[A-Z a-z 0-9]{8,}$";
+//            Pattern pattern4 = Pattern.compile(regex4);
+//            Matcher matcher4 = pattern4.matcher(password);
+//            boolean r = matcher4.matches();
+//            return r;
+//        }
+
+    //Exception Handled for Password Rule 1 : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex4 = "^[A-Z a-z]{8,}$";
+
+    public boolean validatePasswordRule1(String password) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex4);
+        if (Pattern.matches(regex4, password) == true)
+            return Pattern.matches(regex4, password);
+        else {
+            throw new UserRegistrationException("Enter password according to rule 1 " +
+                    "(Minimum 8 characters)");
+        }
     }
 
     //Junit UC6: Take Password (Password At least Eight Character one Upper case) And
     // Check It Is Getting Matched With Regex/Regular
 
-    public boolean validatePasswordRule2(String password) {
-        String regex5 = "^[A-Z]{1}+[a-z 0-9]{7,}$";
-        Pattern pattern5 = Pattern.compile(regex5);
-        Matcher matcher5 = pattern5.matcher(password);
-        boolean r = matcher5.matches();
-        return r;
+//        public boolean validatePasswordRule2 (String password){
+//            String regex5 = "^[A-Z]{1}+[a-z 0-9]{7,}$";
+//            Pattern pattern5 = Pattern.compile(regex5);
+//            Matcher matcher5 = pattern5.matcher(password);
+//            boolean r = matcher5.matches();
+//            return r;
+//        }
+
+    //Exception Handled for Password Rule 2 : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex5 = "^[A-Z]{1}+[a-z]{8,}$";
+
+    public boolean validatePasswordRule2(String password) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex5);
+        if (Pattern.matches(regex5, password) == true)
+            return Pattern.matches(regex5, password);
+        else {
+            throw new UserRegistrationException("Enter password according to rule 2 " +
+                    "(Password At least Eight Character one Upper case)");
+        }
     }
 
     //Junit UC7: Take Password (Password At least one Upper case and one numeric) And
     // Check It Is Getting Matched With Regex/Regular
 
-    public boolean validatePasswordRule3(String password) {
-        String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]+{1,}$";
-        Pattern pattern6 = Pattern.compile(regex6);
-        Matcher matcher6 = pattern6.matcher(password);
-        boolean r = matcher6.matches();
-        return r;
+//        public boolean validatePasswordRule3 (String password){
+//            String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]+{1,}$";
+//            Pattern pattern6 = Pattern.compile(regex6);
+//            Matcher matcher6 = pattern6.matcher(password);
+//            boolean r = matcher6.matches();
+//            return r;
+//        }
+
+    //Exception Handled for Password Rule 3 : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex6 = "^[A-Z]{1}+[a-z]{8,}$";
+
+    public boolean validatePasswordRule3(String password) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex6);
+        if (Pattern.matches(regex6, password) == true)
+            return Pattern.matches(regex6, password);
+        else {
+            throw new UserRegistrationException("Enter password according to rule 3 " +
+                    "(Password At least one Upper case and one numeric)");
+        }
     }
 
     //Junit UC8: Take Password (Password At least One Upper Case & One Numeric & One Special Character) And
     // Check It Is Getting Matched With Regex/Regular
 
-    public boolean validatePasswordRule4(String password) {
-        String regex7 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
-        Pattern pattern7 = Pattern.compile(regex7);
-        Matcher matcher7 = pattern7.matcher(password);
-        boolean r = matcher7.matches();
-        return r;
+//        public boolean validatePasswordRule4 (String password){
+//            String regex7 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
+//            Pattern pattern7 = Pattern.compile(regex7);
+//            Matcher matcher7 = pattern7.matcher(password);
+//            boolean r = matcher7.matches();
+//            return r;
+//        }
+
+    //Exception Handled for Password Rule 4 : throw custom exceptions in case of Invalid User Details
+
+    private static final String regex7 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
+
+    public boolean validatePasswordRule4(String password) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex7);
+        if (Pattern.matches(regex7, password) == true)
+            return Pattern.matches(regex7, password);
+        else {
+            throw new UserRegistrationException("Enter password according to rule 4 " +
+                    "(Password At least One Upper Case & One Numeric & One Special Character)");
+        }
     }
 
     //Junit UC9: Take Email And Check It Is Getting Matched With Regex/Regular
+//
+//        public boolean emailSamples (String emailSample){
+//            String regex8 = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
+//            Pattern pattern8 = Pattern.compile(regex8);
+//            Matcher matcher8 = pattern8.matcher(emailSample);
+//            boolean r = matcher8.matches();
+//            return r;
+//        }
+    //Exception Handled for Email checking: throw custom exceptions in case of Invalid User Details
 
-    public boolean emailSamples(String emailSample) {
-        String regex8 = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
-        Pattern pattern8 = Pattern.compile(regex8);
-        Matcher matcher8 = pattern8.matcher(emailSample);
-        boolean r = matcher8.matches();
-        return r;
+    private static final String regex8 = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
+
+    public boolean emailSamples(String emailSample) throws UserRegistrationException {
+        Pattern pattern = Pattern.compile(regex8);
+        if (Pattern.matches(regex8, emailSample) == true)
+            return Pattern.matches(regex8, emailSample);
+        else {
+            throw new UserRegistrationException("Invalid Email format");
+        }
     }
 }
--- src/test/java/com/bridgelabz/UserRegisterationTest.java
+++ src/test/java/com/bridgelabz/UserRegisterationTest.java
@@ -5,58 +5,60 @@ import org.junit.jupiter.api.Test;
 
 public class UserRegisterationTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+    public void givenFirstName_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateFirstName("Yogesh");
+        boolean result;
+        result = userRegistration.validateFirstName("yogesh");
         Assertions.assertTrue(result);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
+    public void givenLastName_WhenProper_ShouldReturnTrue() throws UserRegistrationException{
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateLastName("More");
+        boolean result;
+        result = userRegistration.validateLastName("More");
         Assertions.assertTrue(result);
     }
 
     @Test
-    public void givenEmail_WhenProper_ShouldReturnTrue() {
+    public void givenEmail_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateEmail("iyogeshmore1998@gmail.com");
         Assertions.assertTrue(result);
     }
 
     @Test
-    public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
+    public void givenMobileNumber_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateMobileNumber("91 8850055197");
         Assertions.assertTrue(result);
     }
     @Test
-    public void givenPasswordRule1_WhenProper_ShouldReturnTrue(){
+    public void givenPasswordRule1_WhenProper_ShouldReturnTrue() throws UserRegistrationException{
         UserRegistration userRegistration =new UserRegistration();
         boolean result = userRegistration.validatePasswordRule1("Programmingforlife");
         Assertions.assertTrue(result);
     }
 @Test
-   public void givenPasswordRule2_WhenProper_ShouldReturnTrue(){
+   public void givenPasswordRule2_WhenProper_ShouldReturnTrue() throws  UserRegistrationException{
     UserRegistration userRegistration = new UserRegistration();
     boolean result = userRegistration.validatePasswordRule2("Yogesh1998");
     Assertions.assertTrue(result);
 }
 @Test
-    public void givenPasswordRule3_WhenProper_ShouldReturnTrue(){
+    public void givenPasswordRule3_WhenProper_ShouldReturnTrue() throws UserRegistrationException{
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validatePasswordRule3("Uzumaki2002");
         Assertions.assertTrue(result);
     }
     @Test
-    public void givenPasswordRule4_WhenProper_ShouldReturnTrue(){
+    public void givenPasswordRule4_WhenProper_ShouldReturnTrue() throws UserRegistrationException{
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validatePasswordRule4("Yogesh@1998");
         Assertions.assertTrue(result);
     }
 @Test
-public void givenemailSamples_WhenProper_ShouldReturnTrue(){
+public void givenemailSamples_WhenProper_ShouldReturnTrue() throws UserRegistrationException{
     UserRegistration userRegistration = new UserRegistration();
     boolean result = userRegistration.emailSamples("Yogesh2021998@gmail.com");
     Assertions.assertTrue(result);
