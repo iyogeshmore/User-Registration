0a24a84059e60e9d343d136c734bb2ed69536ccc
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-18 00:42:17 +0530

    Added : Exception for all test cases

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -142,16 +142,6 @@ public class UserRegistration {
             System.out.println("Invalid G-mail Address");
     }
 
-    //Junit UC1: Take First Name And Check It Is Getting Matched With Regex/Regular
-
-//    public boolean validateFirstName(String fname) {
-//        String regex = "^[A-Z]{1}[a-z]*";
-//        Pattern p = Pattern.compile(regex);
-//        Matcher m = p.matcher(fname);
-//        boolean r = m.matches();
-//        return r;
-//    }
-
     //Exception Handled for First name : throw custom exceptions in case of Invalid User Details
 
     private static final String regex = "^[A-Z]{1}[a-z]*";
@@ -164,16 +154,6 @@ public class UserRegistration {
             throw new UserRegistrationException("Send a proper First Name");
         }
     }
-
-    //Junit UC2: Take Last Name And Check It Is Getting Matched With Regex/Regular
-
-//    public boolean validateLastName(String lName) {
-//        String regex1 = "^[A-Z]{1}[a-z]{2,}$";
-//        Pattern pattern1 = Pattern.compile(regex1);
-//        Matcher matcher1 = pattern1.matcher(lName);
-//        boolean r = matcher1.matches();
-//        return r;
-//    }
 
     //Exception Handled for Last name : throw custom exceptions in case of Invalid User Details
 
@@ -188,17 +168,6 @@ public class UserRegistration {
         }
     }
 
-    //Junit UC3: Take Email And Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validateEmail (String email){
-//            String regex2 = "^[a-zA-Z0-9]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
-//            // {1,}^[a-z]+[0-9]+[@]+gmail.com
-//            Pattern pattern2 = Pattern.compile(regex2);
-//            Matcher matcher2 = pattern2.matcher(email);
-//            boolean r = matcher2.matches();
-//            return r;
-//        }
-
     //Exception Handled for Email : throw custom exceptions in case of Invalid User Details
 
     private static final String regex2 = "^[a-zA-Z0-9]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]+)*$";
@@ -212,16 +181,6 @@ public class UserRegistration {
         }
     }
 
-    //Junit UC4: Take Mobile Number And Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validateMobileNumber (String mobileNo){
-//            String regex3 = "(91|0)?\\s?[6-9][0-9]{9}$";
-//            Pattern pattern3 = Pattern.compile(regex3);
-//            Matcher matcher3 = pattern3.matcher(mobileNo);
-//            boolean r = matcher3.matches();
-//            return r;
-//        }
-
 //Exception Handled for Mobile Number : throw custom exceptions in case of Invalid User Details
 
     private static final String regex3 = "(91|0)?\\s?[6-9][0-9]{9}$";
@@ -234,16 +193,6 @@ public class UserRegistration {
             throw new UserRegistrationException("Send a proper Mobile Number");
         }
     }
-
-    //Junit UC5: Take Password (Minimum 8 characters) And Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validatePasswordRule1 (String password){
-//            String regex4 = "^[A-Z a-z 0-9]{8,}$";
-//            Pattern pattern4 = Pattern.compile(regex4);
-//            Matcher matcher4 = pattern4.matcher(password);
-//            boolean r = matcher4.matches();
-//            return r;
-//        }
 
     //Exception Handled for Password Rule 1 : throw custom exceptions in case of Invalid User Details
 
@@ -259,17 +208,6 @@ public class UserRegistration {
         }
     }
 
-    //Junit UC6: Take Password (Password At least Eight Character one Upper case) And
-    // Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validatePasswordRule2 (String password){
-//            String regex5 = "^[A-Z]{1}+[a-z 0-9]{7,}$";
-//            Pattern pattern5 = Pattern.compile(regex5);
-//            Matcher matcher5 = pattern5.matcher(password);
-//            boolean r = matcher5.matches();
-//            return r;
-//        }
-
     //Exception Handled for Password Rule 2 : throw custom exceptions in case of Invalid User Details
 
     private static final String regex5 = "^[A-Z]{1}+[a-z]{8,}$";
@@ -283,17 +221,6 @@ public class UserRegistration {
                     "(Password At least Eight Character one Upper case)");
         }
     }
-
-    //Junit UC7: Take Password (Password At least one Upper case and one numeric) And
-    // Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validatePasswordRule3 (String password){
-//            String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]+{1,}$";
-//            Pattern pattern6 = Pattern.compile(regex6);
-//            Matcher matcher6 = pattern6.matcher(password);
-//            boolean r = matcher6.matches();
-//            return r;
-//        }
 
     //Exception Handled for Password Rule 3 : throw custom exceptions in case of Invalid User Details
 
@@ -309,17 +236,6 @@ public class UserRegistration {
         }
     }
 
-    //Junit UC8: Take Password (Password At least One Upper Case & One Numeric & One Special Character) And
-    // Check It Is Getting Matched With Regex/Regular
-
-//        public boolean validatePasswordRule4 (String password){
-//            String regex7 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
-//            Pattern pattern7 = Pattern.compile(regex7);
-//            Matcher matcher7 = pattern7.matcher(password);
-//            boolean r = matcher7.matches();
-//            return r;
-//        }
-
     //Exception Handled for Password Rule 4 : throw custom exceptions in case of Invalid User Details
 
     private static final String regex7 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
@@ -334,15 +250,6 @@ public class UserRegistration {
         }
     }
 
-    //Junit UC9: Take Email And Check It Is Getting Matched With Regex/Regular
-//
-//        public boolean emailSamples (String emailSample){
-//            String regex8 = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
-//            Pattern pattern8 = Pattern.compile(regex8);
-//            Matcher matcher8 = pattern8.matcher(emailSample);
-//            boolean r = matcher8.matches();
-//            return r;
-//        }
     //Exception Handled for Email checking: throw custom exceptions in case of Invalid User Details
 
     private static final String regex8 = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
